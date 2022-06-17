9efe7b6491f0466419e711d97d070e9ad5292a26
linnefromice <linnefromice@gmail.com>
2022-06-17 10:34:12 +0900

    tx: step3

--- aptos.dev/first-transaction/index.ts
+++ aptos.dev/first-transaction/index.ts
@@ -166,3 +166,27 @@ export class RestClient {
     return await this.executeTransactionWithPayload(accountFrom, payload)
   }
 }
+
+/** Faucet creates and funds accounts. This is a thin wrapper around that */
+export class FaucetClient {
+  url: string
+  restClient: RestClient
+
+  constructor(url: string, restClient: RestClient) {
+    this.url = url
+    this.restClient = restClient
+  }
+
+  /** This creates a account if it does not exist and mints the specified amount of coins into that account */
+  async fundAccount(address: string, amount: number) {
+    const url = `${this.url}/mint?amount=${amount}&address=${address}`
+    const response = await fetch(url, { method: "POST" })
+    if (response.status != 200) {
+      assert(response.status == 200, await response.text())
+    }
+    const tnxHashes = (await response.json()) as Array<string>
+    for (const tnxHash of tnxHashes) {
+      await this.restClient.waitForTransaction(tnxHash)
+    }
+  }
+}
