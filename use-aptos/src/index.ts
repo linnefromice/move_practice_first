94dca033da22859f9bb0890daf49807f6372aae0
linnefromice <linnefromice@gmail.com>
2022-06-29 12:05:13 +0900

    clean

--- use-aptos/src/index.ts
+++ use-aptos/src/index.ts
@@ -3,14 +3,12 @@ import { NODE_URL } from "./constants"
 
 const main = async () => {
   const client = new AptosClient(NODE_URL)
-  const results = await Promise.all([
-    client.getChainId(),
+  const [ledgerInfo, transactions] = await Promise.all([
     client.getLedgerInfo(),
     client.getTransactions()
   ])
-  console.log(results[0])
-  console.log(results[1])
-  console.log(results[2])
+  console.log(ledgerInfo)
+  console.log(transactions)
 }
 
 main()
